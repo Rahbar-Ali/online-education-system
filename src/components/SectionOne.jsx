0f5d7b080a774fd506dc2507fb912dc8288770a4
Rahbar Ali <112726734+Rahbar-Ali@users.noreply.github.com>
2025-02-20 01:12:41 +0500

    update

--- src/components/SectionOne.jsx
+++ src/components/SectionOne.jsx
@@ -9,7 +9,7 @@ const SectionOne = () => {
   return (
     <div className=" flex w-full  md:flex-row flex-col  h-screen max-h-[700px] ">
       <div className="xl:w-2/2 flex  items-center">
-        <div className="w-full flex md:justify-end sm:justify-center mr-0 2xl:mr-12 mt-10">
+        <div className="w-full flex md:justify-end sm:justify-center mr-0 2xl:mr-12 md:mt-6 mt-14">
           <div
             className="flex justify-center items-center  relative overflow-hidden"
             style={{ objectFit: "cover" }}
@@ -53,18 +53,18 @@ const SectionOne = () => {
           </div>
         </div>
       </div>
-      <div className="w-full flex h-full flex-col justify-center ">
+      <div className="w-full flex h-full flex-col md:justify-center ">
         <div className=" w-full flex flex-col items-center md:items-start">
-          <h1 className="2xl:w-[70%] xl:text-6xl lg:text-5xl sm:mt-2 sm:text-4xl sm:mb-3  text-4xl font-bold mb-3 md:mb-8 text-center md:text-left">
+          <h1 className="2xl:w-[70%] xl:text-6xl lg:text-5xl sm:mt-2 sm:text-4xl sm:mb-3  text-4xl font-bold mb-3 md:mb-8 text-center md:text-left mt-6 md:mt-0">
             Turn weaknesses into strengths
           </h1>
-          <p className="xl:text-2xl text-sm sm:mb-4 px-2 sm:text-lg mb-4 md:mb-8 md:pr-10 text-center md:text-left">
+          <p className="xl:text-2xl text-sm sm:mb-4 px-2 sm:text-lg mb-4 md:mb-8 md:pr-10 text-center md:text-left mt-4 md:mt-0">
             Leaders Academy’s structured courses in Web Development, Grpahics
             Designing, and Office adapt to your progress, drilling harder
             concepts with precision timing to ensure you master them
             efficiently.
           </p>
-          <div className="w-55 h-17 bg-amber-500 ml-4 text-2xl text-center p-4">
+          <div className="w-55 h-17 bg-amber-500 ml-4 text-2xl text-center p-4 mt-4 md:mt-0">
             VIEW COURSES
           </div>
         </div>
--- src/components/SectionTwo.jsx
+++ src/components/SectionTwo.jsx
@@ -1,22 +1,30 @@
 import img1 from "../assets/img3.jpg";
 import { MdOutlineSlowMotionVideo } from "react-icons/md";
 import { MdBookOnline } from "react-icons/md";
-
+import {
+  BsBackpack2,
+  BsBriefcase,
+  BsCardChecklist,
+  BsChatRightText,
+  BsClipboard2Heart,
+  BsCalendar2Day,
+  BsInfinity,
+} from "react-icons/bs";
 import { FaCalendarAlt, FaInfinity } from "react-icons/fa";
 
 const SectionTwo = () => {
   return (
-    <div className="flex  h-full md:h-screen md:max-h-[700px] flex-col justify-center md:flex-row bg-gray-500 ">
-      <div className="flex lg:p-12 lg:w-2/2 md:w-1/2 flex-col w-full justify-center items-center ">
-        <h1 className="mt-2 lg:text-6xl   md:text-3xl text-2xl text-center text-white font-bold">
+    <div className="flex  h-full md:h-screen md:max-h-[700px] flex-col justify-center md:flex-row bg-gray-500 md:pt-10 ">
+      <div className="flex lg:px-12 lg:w-2/2 md:w-1/2 flex-col w-full justify-center items-center ">
+        <h1 className="md:mt-2 lg:text-6xl mt-10 px-2 md:px-0 md:text-3xl text-2xl md:text-center md:ml-0 ml-6 text-white font-bold">
           World's cheapest online learning platform designed for Pakistan Youth
         </h1>
         <img
           src={img1}
           alt="Transparent Laptop Mac - Blank Laptop Screen@nicepng.com"
-          className=" lg:mt-12 mt-4 md:mt-8 w-[320px] h-[300px]"
+          className=" hidden md:block lg:mt-12 mt-4 md:mt-8 w-[320px] h-[300px]"
         />
-        <div className="   flex mt-4 md:mt-6">
+        <div className="  hidden  md:flex mt-4 md:mt-6">
           <input
             type="button"
             className="rounded-sm mr-2 w-38 h-10 bg-black text-white"
@@ -29,24 +37,48 @@ const SectionTwo = () => {
           ></input>
         </div>
       </div>
-      <div className="flex  lg:gap-10 gap-2 lg:w-2/3 md:gap-10 flex-col justify-center text-white">
-        <div className="flex md:justify-start items-center justify-center text-3xl">
-          <FaCalendarAlt className="lg:text-5xl  text-5xl" />
-          <div className="ml-2 text-xl">Online weekly classes on Zoom</div>
-        </div>
-        <div className="flex md:justify-start items-center justify-center text-3xl">
-          <FaInfinity className="lg:text-5xl  text-5xl" />
-          <div className="ml-2 text-xl">
-            Lifetime access to HD recorded videos
+      <div className="flex  lg:gap-10 gap-8 lg:w-2/3 md:gap-10 mt-8 md:mt-0 ml-8 md:ml-0 md:px-0 flex-col justify-center  text-white">
+        <div className="flex md:justify-start items-center   text-3xl">
+          <BsCalendar2Day className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg ">
+            <b>Online</b> weekly classes on Zoom
           </div>
         </div>
-        <div className="flex md:justify-start items-center justify-center text-3xl">
-          <MdOutlineSlowMotionVideo className="lg:text-5xl  text-5xl" />
-          <div className="ml-2 text-xl">Lorem ipsum dolor sit amet.</div>
+        <div className="flex md:justify-start items-center   text-3xl">
+          <BsInfinity className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Lifetime</b> access to HD recorded videos
+          </div>
         </div>
-        <div className="flex md:justify-start items-center justify-center text-3xl">
-          <MdOutlineSlowMotionVideo className="lg:text-5xl  text-5xl" />
-          <div className="ml-2 text-xl">Lorem ipsum dolor sit amet.</div>
+        <div className="flex md:justify-start items-center  text-3xl">
+          <BsBackpack2 className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Premium</b> course materials
+          </div>
+        </div>
+        <div className="flex md:justify-start items-center  text-3xl">
+          <BsBriefcase className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Internship</b> and Job placement support
+          </div>
+        </div>
+        <div className="flex md:justify-start items-center   text-3xl">
+          <BsCardChecklist className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Hands-on</b> task, assignments and real projects
+          </div>
+        </div>
+        <div className="flex md:justify-start items-center   text-3xl">
+          <BsChatRightText className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Q&A</b> support via zoom
+          </div>
+        </div>
+        <div className="flex md:justify-start items-center   text-3xl">
+          <BsClipboard2Heart className="lg:text-5xl  text-4xl" />
+          <div className="ml-4 md:text-xl text-lg">
+            <b>Elite</b> Alumni Community and Leadership opportunities
+          </div>
         </div>
       </div>
     </div>
